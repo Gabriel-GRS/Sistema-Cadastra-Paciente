5b0cde085003ea4ef1cba201d7447273a5c45f39
Gabriel <gabrielrs.job@gmail.com>
2022-02-12 00:01:42 -0300

    Projeto concluído e adiconada requisição com AJAX

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,17 @@
+var botaoBuscar = document.querySelector("#buscar-paciente");
+botaoBuscar.addEventListener("click", function() {
+    console.log("Buscando pacientes")
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function(){
+        var resposta = xhr.responseText;
+        var pacientes = JSON.parse(resposta);
+
+        pacientes.forEach( function(paciente){
+            adicionaPacienteNaTabela(paciente);
+        })
+    })
+    xhr.send();
+});
